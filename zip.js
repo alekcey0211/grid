7399686dc3190cd14e9742278283d9cc89e778a0
alekcey0211 <alekcey_ezh@mail.ru>
2020-12-28 18:41:38 +0300

    .gitignore is now working

--- zip.js
+++ zip.js
@@ -1,6 +1,6 @@
 const zipFolder = require('zip-folder');
 let folderName = 'dist';
-let zipName = 'C:/DATA/WEB_2020/chrome_extension/grid/grid-extension.zip';
+let zipName = 'C:/DATA/WEB_2020/chrome_extension/grid/dist/grid-extension.zip';
 
 zipFolder(folderName, zipName, function (err) {
 	if (err) {
